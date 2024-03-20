05fbd1ed8f0f32ecb64d33162c5acd8f94decd67
supiansauri <114654547+supiansauri@users.noreply.github.com>
2024-03-20 21:47:15 +0700

    first commit

---
+++ tugas/Person.java
@@ -0,0 +1,24 @@
+package tugas;
+
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    };
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String toString() {
+        return name + "(" + address + ")";
+    }
+
+};
